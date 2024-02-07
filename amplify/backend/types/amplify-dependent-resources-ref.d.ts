9841cbf3b689d8a82fec15128fb4947ba95b77ca
Samuel Velasquez <sjv5866@g.rit.edu>
2024-02-06 21:13:07 -0500

    added graphql backend (required assistance from Amplify Dev Docs due to outdated code snippit in the official tutorial)

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "swen614amplifyfullst": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "swen614amplifyfullstabcbd818": {
       "AppClientID": "string",
